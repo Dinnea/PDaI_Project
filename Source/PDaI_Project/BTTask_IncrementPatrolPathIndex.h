40152538482913519da9fbf09e53d3c8561c4a25
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2024-11-19 16:00:55 +0200

    feat: patrolling enemy

--- Source/PDaI_Project/BTTask_IncrementPatrolPathIndex.h
+++ Source/PDaI_Project/BTTask_IncrementPatrolPathIndex.h
@@ -13,5 +13,17 @@ UCLASS()
 class PDAI_PROJECT_API UBTTask_IncrementPatrolPathIndex : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
+
+public:
+	explicit UBTTask_IncrementPatrolPathIndex(FObjectInitializer const& objectInitializer);
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& ownerComponent, uint8* nodeMemory) override;
+
+private:
+	enum class EDirection {Forward, Reverse};
+
+	EDirection direction = EDirection::Forward;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta=(AllowPrivateAccess="true"))
+	bool biDirectional = false;
 	
 };
